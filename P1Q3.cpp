36dc9b8cdeb6fedf6214cf58f0b7135b46b5960f
jeslinpjames <jeslinpjames007@gmail.com>
2023-06-29 09:42:22 +0530

    P1Q3

---
+++ P1Q3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void Reverse(const string& s, int i){
+    if (i<0)
+    return;
+    else 
+    {
+        cout<<s[i];
+        return(Reverse(s,i-1));
+    }
+}
+int main(){
+    string S;
+    cout<<"Enter the String : ";
+    cin>>S;
+    int l = S.length();
+    cout<<"The reverse of "<<S<<" is : ";
+    Reverse(S,l);
+    cout<<endl;
+    return 0;
+}
+// Enter the String : Helloworld
+// The reverse of Helloworld is : dlrowolleH
